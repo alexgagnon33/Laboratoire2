019cfdde17ecaaa62ec71b49416db7eaa1ae08f2
Alexandre Gagnon <116171801+alexgagnon33@users.noreply.github.com>
2022-11-21 10:28:43 -0500

    Update Laboratoire2.py

--- Laboratoire2.py
+++ Laboratoire2.py
@@ -1,11 +1,25 @@
 #Étape 1:
+#lire doc os
+#lire les doc
 
 #Étape 2:
+#afficher les étapes 3 à 6
+
 
 #Étape 3:
+#trouver le max temps
+#moyenne temps chaque équipe mais 
 
 #Étape 4:
+#trouver équart type de chaque équipe 
+#trouver l'équart type le plus petit min
 
 #Étape 5:
+#sous-menu
+#création d'un nouveau doc os
+#ajouter faute dans nouveau doc
 
 #Étape 6:
+#lire info étape 3
+#créer nouveau doc os
+#mettre info étape 3
